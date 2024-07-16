3083b8c1024da00cc3805b49e3b1b1572a47ae65
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-07-15 23:04:03 -0300

    feat(user): configuration entity

---
+++ src/shared/enums/e-core.ts
@@ -0,0 +1,13 @@
+export enum EPermission {
+    ADMINISTRATOR = 'administrator',
+    EDITOR = 'editor',
+    CLIENT = 'client'
+}
+
+export enum EUserStatus {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+    SUSPENDED = 'suspended',
+    PENDING = 'pending',
+    DEACTIVATED = 'deactivated',
+  }
